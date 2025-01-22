7ce687a72359f2108277bb1cf4f609f7453a5ad6
Nicolas Anderson <nicolas-31@outlook.com>
2025-01-22 08:26:29 +0100

    Add initial implementation of HeartRateZoneService and ClientGateway with configuration files

---
+++ exercises/ClientGateway/Controllers/ClientGatewayController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.Metrics;
+using System.Net;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+namespace ClientGateway.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientGatewayController : ControllerBase
+{
+    private string BiometricsImportedTopicName = "RawBiometricsImported";
+    private readonly IProducer<String, String> _producer;
+    private readonly ILogger<ClientGatewayController> _logger;
+
+    public ClientGatewayController(IProducer<String, String> producer, ILogger<ClientGatewayController> logger)
+    {
+        _logger = logger;
+        _producer = producer;
+        logger.LogInformation("ClientGatewayController is Active.");
+    }
+
+    [HttpGet("Hello")]
+    [ProducesResponseType(typeof(String), (int)HttpStatusCode.OK)]
+    public String Hello()
+    {
+        _logger.LogInformation("Hello World");
+        return "Hello World";
+    }
+
+    [HttpPost("Biometrics")]
+    [ProducesResponseType(typeof(String), (int)HttpStatusCode.OK)]
+    public async Task<AcceptedResult> RecordMeasurements(string metrics)
+    {
+        _logger.LogInformation("Accepted biometrics");
+        var message = new Message<String, String>
+        {
+            Value = metrics
+        };
+        var result = await _producer.ProduceAsync(BiometricsImportedTopicName, message);
+        _producer.Flush();
+        return Accepted("", metrics);
+    }
+}
+
+
+
---
+++ exercises/ClientGateway/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClientGateway;
+using ClientGateway.Controllers;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// TODO: Load a config of type ProducerConfig from the "Kafka" section of the config:
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection("Kafka"));
+
+
+// TODO: Register an IProducer of type <String, String>:
+builder.Services.AddSingleton<IProducer<string, string>>(sp =>
+{
+    var config = sp.GetRequiredService<IOptions<ProducerConfig>>().Value;
+    return new ProducerBuilder<string, string>(config).Build();
+});
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
